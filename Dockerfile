d4ebbf9bb41155beabf43b62467656c93941d0b6
Kevin Cashman <kevinjcash@gmail.com>
2015-12-22 09:13:41 -0500

    Initial commit of small Hello Flask app
    Docker file to build image, and travis yaml to run build process.

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# serviceA
+FROM debian
+MAINTAINER Kevin Cashman
+
+EXPOSE 5000
+
+RUN apt-get update && apt-get install -y \
+        python \
+        python-dev \
+        python-pip \
+    && apt-get clean
+RUN pip install flask requests
+RUN mkdir /opt/app
+
+ADD hello.py /opt/app/hello.py
+
+CMD ["/usr/bin/python", "/opt/app/hello.py"]
---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    try:
+        r = requests.get('http://172.31.42.41:5000')
+        return "Hello, {}".format(r.text)
+    except:
+        return "Service B unreachable"
+
+@app.route("/health")
+def health():
+    r = requests.get('http://172.31.42.41:5000')
+    import pdb; pdb.set_trace()
+    return str(r.status_code)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
