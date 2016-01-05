d236871c990ce3b0b971a7e56dbdb9e92fc739d9
Kevin Cashman <kevinjcash@gmail.com>
2016-01-05 01:41:41 -0600

    Add timeout to requests

--- hello.py
+++ hello.py
@@ -7,15 +7,19 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     try:
-        r = requests.get('http://' + str(sys.argv[1]))
+        r = requests.get('http://' + str(sys.argv[1]), timeout=3.0)
         return "Hello, {}".format(r.text)
     except:
         return "Service B unreachable"
 
 @app.route("/health")
 def health():
-    r = requests.get('http://' + str(sys.argv[1]))
-    return str(r.status_code)
+    try:
+        r = requests.get('http://' + str(sys.argv[1]), timeout=3.0)
+        return str(200)
+    except:
+        return str(503)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=False)
+
